68281be3b3dd38222f8a8657784b3f228a67c328
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-15 12:48:36 +0000

    Add files via upload

--- admin_to_id.py
+++ admin_to_id.py
@@ -3,23 +3,29 @@ import json
 with open('res.json') as json_file:
     data = json.load(json_file)
 
-# for hit in data['hits']['hits']:
-#     source = hit['_source'].items()
-
-#     for key, value in source:
-#         print(f'{key} : {value}')
-#         if key == '@admin':
-#             source.insert(0, ('id', value['id']))
-
 def prepend(lst):
+    '''
+    Rebuild dictionary and prepend ID as first element
+    '''
     new_list = []
     print('list', lst)
+
     for dictionary in lst:
         print('dict', dictionary)
+
+        #Create a new dictionary entry at the end of the dictionary 
         dictionary.update({'id': dictionary['@admin']['id']})
+
+        # Remove old entry
         dictionary.pop('@admin')
+
+        #Get all dictionary keys in order 
         keys = list(dictionary.keys())
+
+        #Remove the recently created 'id' field and place it at the front of the list
         keys.insert(0, keys.pop(keys.index('id')))
+
+        #Rebuild the dictionary according to old key order 
         new = [{key: dictionary[key]} for key in keys]
         new_list.append(new)
     return new_list
@@ -32,18 +38,7 @@ for hit in data["hits"]["hits"]:
             source['creation']['maker'] = prepend(source.get("creation").get("maker"))
         if 'classification' in source:
             source['classification'] = prepend(source.get('classification'))
-            # new_list = []
-            # for classification in source.get('classification'):
-            #     new_list.append(prepend(classification))
-            # source['classification'] = new_list
 
-
-# print(data['hits']['hits'])
-
-# for value in data['hits']['hits']:
-#     source = value['_source']
-#     source['@admin'] = source['@admin']['id']
-#     source['id'] = source.pop('@admin')
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile, indent=2)
