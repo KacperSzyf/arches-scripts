fde61159b1a3fe90b7cf1c5c5a192d7e6c6845df
KacperSzyf <kacper.szyf@icloud.com>
2022-01-31 15:26:29 +0000

    Added multi file functionality + comments

--- fix_image_imports.py
+++ fix_image_imports.py
@@ -16,20 +16,37 @@ condition_image = 'Condition Image' #in heritage asses
 document = 'Document' #in information resource
 
 if image in file.columns:
-    for path in file[image].iteritems():
+    for path in file[image].iteritems():    # Loop over all cells within the image column
         if path[1]:
-            file.at[path[0], image] = dict(eval(path[1])[0])['url']
+            file_list = list(eval(path[1])) #Cast path[1] string to a list of dictionaries
+            path_string = ""
+            for fp in file_list:
+                path_string += fp['url'] + ',' #Create a comma separated string for all file paths
+            path_string = path_string[:-1]  #Remove the trailing comma
+            file.at[path[0], image] = path_string #Replace current cell content with path string
+
     file.to_csv(f'{file_name}')
+
 
 elif condition_image in file.columns:
     for path in file[condition_image].iteritems():
         if path[1]:
             if len(eval(path[1])) > 0:
-                file.at[path[0], condition_image] = dict(eval(path[1])[0])['url']
+                file_list = list(eval(path[1])) #Cast path[1] string to a list of dictionaries
+                path_string = ""
+                for fp in file_list:
+                    path_string += fp['url'] + ',' #Create a comma separated string for all file paths
+                path_string = path_string[:-1]  #Remove the trailing comma
+                file.at[path[0], image] = path_string #Replace current cell content with path string
     file.to_csv(f'{file_name}')
 
 elif document in file.columns:
     for path in file[document].iteritems():
         if path[1]:
-            file.at[path[0], document] = dict(eval(path[1])[0])['url']
+            file_list = list(eval(path[1])) #Cast path[1] string to a list of dictionaries
+            path_string = ""
+            for fp in file_list:
+                path_string += fp['url'] + ',' #Create a comma separated string for all file paths
+            path_string = path_string[:-1]  #Remove the trailing comma
+            file.at[path[0], image] = path_string #Replace current cell content with path string
     file.to_csv(f'{file_name}')
